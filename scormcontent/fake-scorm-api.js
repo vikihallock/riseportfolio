cb389dd0aef6bb3f0256d45bae6c46a39233dda2
vikihallock <vikihallock@gmail.com>
2025-11-07 15:07:43 -0600

    Add files via upload

---
+++ scormcontent/fake-scorm-api.js
@@ -0,0 +1,10 @@
+window.API = {
+  LMSInitialize: function() { return "true"; },
+  LMSFinish: function() { return "true"; },
+  LMSGetValue: function() { return ""; },
+  LMSSetValue: function() { return "true"; },
+  LMSCommit: function() { return "true"; },
+  LMSGetLastError: function() { return "0"; },
+  LMSGetErrorString: function() { return "No error"; },
+  LMSGetDiagnostic: function() { return "No diagnostic"; }
+};
